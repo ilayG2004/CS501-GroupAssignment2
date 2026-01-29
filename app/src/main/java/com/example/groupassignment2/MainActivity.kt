37947b12a35a35f0a8179049a08356262c77b441
ilay2004@bu.edu <ilay2004@bu.edu>
2026-01-29 16:18:14 -0500

    Peer programming complete: Implemented text which changes using state and a switch composable.

--- app/src/main/java/com/example/groupassignment2/MainActivity.kt
+++ app/src/main/java/com/example/groupassignment2/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.groupassignment2.ui.theme.GroupAssignment2Theme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +25,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GroupAssignment2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DemoScreen()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CustomText(s: String){
+    Text(text=s)
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    GroupAssignment2Theme {
-        Greeting("Android")
+fun DemoScreen(){
+    val textPossibilties = arrayOf("Awesome HW2 Group App", "Text just got switched!")
+    var changing_text by remember{ mutableStateOf (textPossibilties[0])}
+    var checked by remember{mutableStateOf(false)}
+    Column() {
+        CustomText(changing_text)
+        Switch(
+            checked = checked,
+            onCheckedChange = {
+                checked = it
+                if (checked){
+                    changing_text = textPossibilties[1]
+                } else {
+                    changing_text = textPossibilties[0]
+                }
+            }
+        )
     }
+}
+
+@Preview
+@Composable
+fun CustomTextPreview(){
+    CustomText("Awesome HW2 Group App")
 }
